7d8e0db34cee2a03a5552720e6ab3a4a4439a538
Arunkumar512 <86590528+Arunkumar512@users.noreply.github.com>
2021-06-27 23:49:35 -0700

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p> copyright by SapeAI @ {new Date().getFullYear()} </p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Info (){
+  return( 
+    <div class="note">
+   <h1>javascript and reactjs</h1>
+   <p> A basic web dev react js bootcamp </p>
+  </div> 
+  );
+}
+
+export default Info;
